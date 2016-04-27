35b17213863670b904da6de055992400b814efaa
LittleStupid <lttlstpd@live.com>
2016-04-27 20:52:03 +0800

    add errors

--- lib/todo.rb
+++ lib/todo.rb
@@ -3,6 +3,10 @@ class TodoItem
   attr_reader :description, :due, :priority
 
   def initialize(description, options={})
+    if( false == valid_priority?( options[:priority] ) )
+     raise UdaciListErrors::InvalidPriorityValue
+    end
+    
     @description = description
     @due = options[:due] ? Date.parse(options[:due]) : options[:due]
     @priority = options[:priority]
@@ -12,5 +16,13 @@ class TodoItem
     format_description( @description ) + "due: " +
     format_date_one( @due ) +
     format_priority( @priority )
+  end
+  
+  def valid_priority?( priority )
+    return true if priority == "high"
+    return true if priority == "midium"
+    return true if priority == "low"
+    
+    return false
   end
 end
--- lib/udacilist.rb
+++ lib/udacilist.rb
@@ -5,15 +5,39 @@ class UdaciList
     @title = options[:title]
     @items = []
   end
+  
+  def valid_type?( type )
+    return true if type == "todo"
+    return true if type == "event"
+    return true if type == "link"
+    
+    return false
+  end
+  
   def add(type, description, options={})
     type = type.downcase
+    
+    if( false == valid_type?( type ) )
+      raise UdaciListErrors::InvalidItemType
+    end
+    
     @items.push TodoItem.new(description, options) if type == "todo"
     @items.push EventItem.new(description, options) if type == "event"
     @items.push LinkItem.new(description, options) if type == "link"
   end
+  
+  def out_of_items_range?( index )
+    index >= @items.length
+  end
+  
   def delete(index)
+    if( true == out_of_items_range?( index ) )
+      raise UdaciListErrors::IndexExceedsListSize
+    end
+    
     @items.delete_at(index - 1)
   end
+  
   def all
     puts "-" * @title.length
     puts @title
